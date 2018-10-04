fa362a073cfbe74e61d5c8a04da26b37db51b544
Samuel Hild <42379292+SamuelAtPurdue@users.noreply.github.com>
2018-10-04 18:40:55 -0400

    Updated Formatter interface
    added essential functions for the formatter classes.
    Formatters are on the way and will be used for formatting the data to the specific data structures.
    These classes will use a factory design pattern.

--- Data/SRAD_JRadio_lib/src/com/psps/Formatter.java
+++ Data/SRAD_JRadio_lib/src/com/psps/Formatter.java
@@ -2,19 +2,34 @@ package com.psps;
 
 /**
  * Created by Samuel Hild on 9/29/2018.
+ * Formatter interface is used to organize the Formatter classes.
+ * These functions must be implemented but more functions could be added in the future.
  */
 public interface Formatter {
 
-    Formatter form(Object... data);
+    /**
+     * packs incoming data to the Formatter instance.
+     * This data is stored as the specified data structure.
+     * @param dataIn Data in the form of a string or an array
+     */
+    void pack(Object... dataIn);
 
-    Object[] unform(Formatter formatter);
+    /**
+     * Unpacks the object to unformatted data
+     * @return unformatted data from the data structure stored in the class.
+     */
+    Object[] unpack();
 
-
+    /**
+     * gets the formatted data structure.
+     * @return formatted data structure dependant on instance of the class.
+     */
+    Object getFormattedData();
 
     /**
      * Simply dumps data to command line for Debugging
      * requires override of toString() for specific implementation
-     * @param dumpData data to be dumped to
+     * @param dumpData data to be dumped to CLI
      */
     static void dump(Formatter dumpData){
         System.out.println(dumpData);
