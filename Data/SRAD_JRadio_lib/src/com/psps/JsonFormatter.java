1f2062c5a094044350e958b9c3bd5d4eff0e8630
Samuel Hild <42379292+SamuelAtPurdue@users.noreply.github.com>
2018-10-05 10:33:33 -0400

    Built and Tested JSON Parser

--- Data/SRAD_JRadio_lib/src/com/psps/JsonFormatter.java
+++ Data/SRAD_JRadio_lib/src/com/psps/JsonFormatter.java
@@ -1,5 +1,8 @@
 package com.psps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Samuel Hild on 10/1/2018.
  * This class will not be instantiated outside the package.
@@ -7,10 +10,44 @@ package com.psps;
  */
 class JsonFormatter implements Formatter {
 
+    private static final String JSON_HEADER = "{";
+    private static final String JSON_CLOSER = "}";
+
+    private String formattedData = "[DATA NOT FOUND]";
 
     @Override
-    public void pack(Object... dataIn) {
+    public void pack(Object... dataIn) throws InvalidDataTypeException{
+        List<String> jsonvalues = new ArrayList<>();
+        for (Object data : dataIn){
+            jsonvalues.add(parseData(data));
+        }
+        this.formattedData = constructJson(jsonvalues);
+    }
+    private String parseData(Object data) throws InvalidDataTypeException{
+        if (data instanceof FlightData) {
+            return parseFlightData((FlightData) data);
+        } if (data instanceof StatusData) {
+            return parseStatusData((StatusData) data);
+        } else {
+            throw new InvalidDataTypeException("could not parse data to JSON.");
+        }
+    }
+    private String parseFlightData(FlightData data){
+        String outdata = String.format("\"%s\":{\"value\" : %d, \"units\" : \"%s\"}",data.getType(), data.getValue(), data.getUnits());
+        return outdata;
+    }
+    private String parseStatusData(StatusData data){
+        String outdata = String.format("\"%s\" : %b", data.getType(), data.getValue());
+        return outdata;
+    }
 
+    private String constructJson(List<String> jsonvalues){
+        StringBuffer jsonoutput = new StringBuffer(JSON_HEADER);
+        for (String value : jsonvalues) {
+                jsonoutput.append(value + ", ");
+        }
+        String output = jsonoutput.replace(jsonoutput.lastIndexOf(","),jsonoutput.lastIndexOf(",")+1, JSON_CLOSER).toString();      //TODO make this less gross
+        return output;
     }
 
     @Override
@@ -19,7 +56,7 @@ class JsonFormatter implements Formatter {
     }
 
     @Override
-    public Object getFormattedData() {
-        return null;
+    public String getFormattedData() {
+        return formattedData;
     }
 }
