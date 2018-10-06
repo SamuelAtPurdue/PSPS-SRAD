563d8dab6a9cf490ded6f945558961ab8f9b829f
thekamakaji <jkaji@purdue.edu>
2018-10-06 01:09:25 +0000

    Blah blah

--- flight_computer/clean/main.py
+++ flight_computer/clean/main.py
@@ -2,7 +2,9 @@ import FGU
 import time
 
 from lsm9ds1 import *
-imu.setup(flight_rate = 10, standby_rate = 1)
-
 from bmp280 import *
 from ugps import *
+
+print()
+
+imu.setup(flight_rate = 10, standby_rate = 1)
--- flight_computer/clean/ugps.py
+++ flight_computer/clean/ugps.py
@@ -2,18 +2,18 @@ import gps as UGPS
 
 def _update(self):
   self.data = self.next()
-  
+
 setattr(UGPS, 'update', _update)
 
 def _get(self, new = 0):
   if new == 1:
     self.update()
-	return self.data()
+    return self.data()
 
 setattr(UGPS, 'get', _get)
 
 # Listen on port 2947 (gpsd) of localhost
 gps = UGPS.gps("localhost", "2947")
-UGPS.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+gps.stream(UGPS.WATCH_ENABLE | UGPS.WATCH_NEWSTYLE)
 
 print("Ultimate GPS is object 'gps'")
