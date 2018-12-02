5cf8e990af6e0646595bea10ba82150f250eca95
Samuel Hild <42379292+SamuelAtPurdue@users.noreply.github.com>
2018-12-02 16:13:08 -0500

    Minor Change to add packet code

--- Data/SRAD_JRadio_lib/src/com/psps/FormatterType.java
+++ Data/SRAD_JRadio_lib/src/com/psps/FormatterType.java
@@ -1,5 +1,12 @@
 package com.psps;
 
 public enum FormatterType{
-    FLIGHT,ERROR
+    FLIGHT(0),ERROR(-1);
+
+    private final int packetCode;
+
+    FormatterType(int code){
+        packetCode = code;
+    }
+
 }
